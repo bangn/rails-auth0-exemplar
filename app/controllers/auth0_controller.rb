1dae115a41888d83a2f67df8bd0020bb2bc0128d
bangn <thebangnguyen@gmail.com>
2024-11-06 13:15:50 +1100

    Fix rubocop

--- app/controllers/auth0_controller.rb
+++ app/controllers/auth0_controller.rb
@@ -4,15 +4,15 @@ class Auth0Controller < ApplicationController
     # In this sample, you will pull the raw_info supplied from the id_token.
     # If the id_token is needed, you can get it from session[:userinfo]['credentials']['id_token'].
     # Refer to https://github.com/auth0/omniauth-auth0#authentication-hash for complete information on 'omniauth.auth' contents.
-    session[:userinfo] = request.env['omniauth.auth']['extra']['raw_info']
+    session[:userinfo] = request.env["omniauth.auth"]["extra"]["raw_info"]
 
-    redirect_to '/dashboard'
+    redirect_to "/dashboard"
   end
 
   # if user authentication fails on the provider side OmniAuth will redirect to /auth/failure,
   # passing the error message in the 'message' request param.
   def failure
-    @error_msg = request.params['message']
+    @error_msg = request.params["message"]
   end
 
   def logout
@@ -25,12 +25,11 @@ class Auth0Controller < ApplicationController
   AUTH0_CONFIG = Rails.application.config.auth0
 
   def logout_url
-
     request_params = {
       returnTo: root_url,
-      client_id: Rails.application.config.auth0['auth0_client_id']
+      client_id: Rails.application.config.auth0["auth0_client_id"]
     }
 
-    URI::HTTPS.build(host: AUTH0_CONFIG['auth0_domain'], path: '/v2/logout', query: request_params.to_query).to_s
+    URI::HTTPS.build(host: AUTH0_CONFIG["auth0_domain"], path: "/v2/logout", query: request_params.to_query).to_s
   end
 end
